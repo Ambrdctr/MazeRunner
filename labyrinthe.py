ccfededfaf589ba122025620cfcd1ac164abe5b5
Gitravail <raphael.tournafond@gmail.com>
2017-12-21 10:32:18 +0100

    Labyrinthe version 2.0 is here !

--- labyrinthe.py
+++ labyrinthe.py
@@ -1,19 +1,4 @@
-
-# coding: utf-8
-
-# # Imports
-
-# In[183]:
-
-from random import *
-import time
-
-
-# # Class
-
-# ## Cell class
-
-# In[184]:
+from random import shuffle
 
 class Cell:
 
@@ -23,14 +8,9 @@ class Cell:
         self.state = 'pierre'
         self.set = i
 
-
-# ## Grid class
-
-# In[185]:
-
 class Grid:
 
-    def __init__(self, h, w):
+    def __init__(self, w, h):
         self.w = w
         self.h = h
         grid = []
@@ -43,8 +23,8 @@ class Grid:
                 grid[i].append(initial_cell)
                 ind += 1
         self.grid = grid
-
-    def is_cell(self,c,wall):
+        
+    def have_neighbor(self,c,wall):
         if wall == 0:
             if c[0] == 0:
                 return False
@@ -58,21 +38,18 @@ class Grid:
             if c[1] == 0:
                 return False
         return True
+    
+    def different_sets(self, cell, neighbor):
+        return self.grid[cell[0]][cell[1]].set != self.grid[neighbor[0]][neighbor[1]].set
 
-    def remove_wall(self,c1, c2, w):
-        #print(wall, (wall+2)%4)
-        #print(c1,wall,c2,(wall+2)%4)
+    def remove_wall(self,c1, c2):
+        w = c1[2]
         self.grid[c1[0]][c1[1]].walls[w] = False
         self.grid[c2[0]][c2[1]].walls[(w+2)%4] = False
 
-
-# ## Set class
-
-# In[186]:
-
 class Set:
 
-    def __init__(self):
+    def __init__(self): # {0: set([(f, o), (o, b), (a, r)...])}
         self.lst = {}
 
     def already(self, ind, c):
@@ -80,119 +57,82 @@ class Set:
             if c in self.lst[ind]:
                 return True
         return False
-
-    def add_set(self, ind, c):
-        if not self.already(ind, c):
-            self.lst[ind] = [c]
-
-    def same_set(self, c1, c2, grid):
-        idc1 = grid.grid[c1[0]][c1[1]].set
-        idc2 = grid.grid[c2[0]][c2[1]].set
-        if idc1 < idc2:
-            for e in self.lst[idc2]:
-                grid.grid[e[0]][e[1]].set = idc1
-            self.lst[idc1] += self.lst[idc2]
-            del self.lst[idc2]
+            
+    def join_sets(self, c1, c2, maze):
+        set1 = maze.grid[c1[0]][c1[1]].set
+        set2 = maze.grid[c2[0]][c2[1]].set
+        if not self.already(set1, c1) and not self.already(set2, c2):
+            self.lst[set1] = set([c1, c2])
+            maze.grid[c2[0]][c2[1]].set = set1
+        elif not self.already(set1, c1):
+            self.lst[set2].add(c1)
+            maze.grid[c1[0]][c1[1]].set = set2
+        elif not self.already(set2, c2):
+            self.lst[set1].add(c2)
+            maze.grid[c2[0]][c2[1]].set = set1
         else:
-            for e in self.lst[idc1]:
-                grid.grid[e[0]][e[1]].set = idc2
-            self.lst[idc2]+= self.lst[idc1]
-            del self.lst[idc1]
+            if len(self.lst[set1]) >= len(self.lst[set2]):
+                for e in self.lst[set2]:
+                    maze.grid[e[0]][e[1]].set = set1
+                setNb = set1
+            else:
+                for e in self.lst[set1]:
+                    maze.grid[e[0]][e[1]].set = set2
+                setNb = set2
+            setInt = set.union(self.lst[set1], self.lst[set2])
+            del self.lst[set1]
+            del self.lst[set2]
+            self.lst[setNb] = setInt
 
-    def different_sets(self, c1, c2, grid):
-        return grid.grid[c1[0]][c1[1]].set != grid.grid[c2[0]][c2[1]].set
 
-    def all_same_set(self, grid, size):
-        return len(self.lst) == 1 and next(iter(self.lst)) == 0 and size == len(self.lst[0])
-
-
-# # Principal program
-
-# In[187]:
-
-def choseAndRemove(set, grid):
-    size = grid.w * grid.h
-    found = False
-    while not set.all_same_set(grid, size):
-        # Choix d'un mur aleatoire
-        x = randint(0, grid.w - 1)
-        y = randint(0, grid.h - 1)
-        wall = randint(0,3)
-        c1 = (x, y)
-        #print(c1, wall)
-        set.add_set(grid.grid[c1[0]][c1[1]].set, c1)
-        # Case derriere le mur
-        if grid.is_cell(c1, wall):
-            if grid.grid[c1[0]][c1[1]].walls[wall] == True:
-                if wall == 0 and set.different_sets(c1, (x-1,y), grid): #top
-                    c2 = (x-1,y)
-                    found = True
-                elif wall == 1 and set.different_sets(c1, (x,y+1), grid): #right
-                    c2 = (x,y+1)
-                    found = True
-                elif wall == 2 and set.different_sets(c1, (x+1,y), grid): #bottom
-                    c2 = (x+1,y)
-                    found = True
-                elif wall == 3 and set.different_sets(c1, (x,y-1), grid): #left
-                    c2 = (x,y-1)
-                    found = True
-                if found:
-                    set.add_set(grid.grid[c2[0]][c2[1]].set, c2)
-                    set.same_set(c1,c2,grid)
-                    grid.remove_wall(c1,c2,wall)
-                    found = False
-                    #print(c2)
-        #print(set.lst)
-        #disp_maze(grid)
-    #print(len(set.lst[0]))
-    #print(set.lst)
-    #disp_maze(grid)
-        
-
-def kruskal(maze):
-    cellSet = Set()
-    choseAndRemove(cellSet,maze)
-    return maze
-
-def disp_maze(maze):
-    chaine = ""
+def makeRandomWallList(maze):
+    list = []
     for i in range(maze.w):
         for j in range(maze.h):
-            chaine += str(maze.grid[i][j].walls) + " " + str(maze.grid[i][j].set)
-        chaine += "\n"
-    return print(chaine)
+            for k in range(4):
+                if maze.have_neighbor((i, j), k):
+                    list.append((i, j, k))
+    shuffle(list)
+    return list
 
-def colnum_string(n):
-    string = ""
-    while n > 0:
-        n, remainder = divmod(n - 1, 26)
-        string = chr(65 + remainder) + string
-    return string
-
-
-    
-
-
-def create_maze(w,h):
-    debut = time.time()
-    maze = Grid(w,h)
-    #disp_maze(maze)
-    kruskal(maze)
-    #disp_maze(maze)
-    fin = time.time()
-    #print('Fini. généré en : ' + str(round(fin-debut,3)) + ' secondes')
-    maze.grid[0][0].state = 'sortie'
+def find_neighbor(cell):
+    x = cell[0]
+    y = cell[1]
+    wall = cell[2]
+    if wall == 0: #top
+        xp = x-1
+        yp = y
+    elif wall == 1: #right
+        xp = x
+        yp = y+1
+    elif wall == 2: #bottom
+        xp = x+1
+        yp = y
+    elif wall == 3: #left
+        xp = x
+        yp = y-1
+    return (xp, yp)
+            
+def kruskal(maze):
+    #STEP I
+    #a-
+    wallList = makeRandomWallList(maze)
+    #print(wallList)
+    #b-
+    cellSet = Set()
+    #STEP II
+    for cell in wallList: #cell with wall
+        #step 1
+        neighbor = find_neighbor(cell)   
+        if maze.different_sets(cell, neighbor):
+            #step a
+            maze.remove_wall(cell, neighbor)
+            #step b
+            cell = (cell[0], cell[1])
+            cellSet.join_sets(cell, neighbor, maze)
     return maze
 
-
-# # Run
-
-# In[188]:
-
-
-
-
-# In[ ]:
-
-
-
+def create_maze(w,h,file):
+    maze = Grid(w,h)
+    kruskal(maze)
+    return maze
